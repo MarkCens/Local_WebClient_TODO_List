d174fb06688d013f69249fbc680dbbc96b302688
MarkCens <cs1053283896@gmail.com>
2026-06-17 18:17:27 +0800

    修复项目路径bug

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ from zhdate import ZhDate
 
 
 app = Flask(__name__)
-DB_DIR = 'database'
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_DIR = os.path.join(BASE_DIR, 'database')
 DB_PATH = os.path.join(DB_DIR, 'todo.db')
 
 if not os.path.exists(DB_DIR):
